38e98c0d66c2b06491fb525f8768de7cb1025bf9
venkykumar0006 <65965126+BoddepallyVenkatesh06@users.noreply.github.com>
2024-02-23 00:29:56 +0530

    Portfolio Template

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// typing animation
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Designer",
+    "Web Developer",
+    "Graphic Designer",
+    "Cyber Security Maintainer",
+  ],
+  typeSpeed: 200,
+  BackSpeed: 20,
+  loop: true,
+});
---
+++ js/style-switcher.js
@@ -0,0 +1,48 @@
+// toggle style switcher
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+
+styleSwitcherToggle.addEventListener("click", () => {
+  document.querySelector(".style-switcher").classList.toggle("open");
+});
+// hide style switcher on scroll
+window.addEventListener("scroll", () => {
+  if (document.querySelector(".style-switcher").classList.contains("open")) {
+    document.querySelector(".style-switcher").classList.remove("open");
+  }
+});
+
+// theme colors
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color) {
+  alternateStyles.forEach((style) => {
+    if (color === style.getAttribute("title")) {
+      style.removeAttribute("disabled");
+    } else {
+      style.setAttribute("disabled", "true");
+    }
+  });
+}
+// setActiveStyle();
+
+// Light and Dark Theme
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+  dayNight.querySelector("i").classList.toggle("fa-sun");
+  dayNight.querySelector("i").classList.toggle("fa-moon");
+  document.body.classList.toggle("dark");
+});
+window.addEventListener("load", () => {
+  if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").classList.add("fa-sun");
+  } else {
+    dayNight.querySelector("i").classList.add("fa-moon");
+  }
+});
+
+// =============active state for nav
+// const nav = document.querySelector(".nav");
+// const navActive = document.querySelector(".navactive");
+// console.log(nav);
+// navActive.addEventListener("click", () => {
+//   style.removeAttribute("navactive");
+// });
